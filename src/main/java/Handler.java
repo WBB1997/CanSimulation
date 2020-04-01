f382135643f508f41463f9dcbfa3504cb3cec571
wubeibei_Desktop <690252189@qq.com>
2020-04-01 22:07:08 +0800

    完成CAN总线模拟程序操作Client

--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -1,4 +1,5 @@
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.eventbus.Subscribe;
 import eventbus.MessageWrap;
@@ -6,6 +7,7 @@ import eventbus.MyEventBus;
 import huat.wubeibei.candataconvert.DataConvert;
 import huat.wubeibei.candataconvert.JSONStreamListener;
 import huat.wubeibei.candataconvert.command.MessageName;
+import util.ByteUtil;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -21,11 +23,11 @@ import java.util.concurrent.Executors;
 public class Handler {
     private DataConvert dataConvert;
     private final static int receivePort = 8888;
-    private final static int sendPort = 9988;
-        private final static String CANIp = "192.168.1.60"; // CAN总线IP地址
+    private final static int sendPort = 9999;
+    private final static String ServiceIp = "192.168.0.101"; // CAN总线IP地址
     private final static int MessageLength = 10;
     private final Thread CanReceiveThread = new Thread(new CanReceive()); // CAN总线接收线程
-    private final HashMap<String, ExecutorService> MapThreadPool = new HashMap<String, ExecutorService>(){ // 接收线程池Map
+    private final HashMap<String, ExecutorService> MapThreadPool = new HashMap<String, ExecutorService>() { // 接收线程池Map
         {
             put(MessageName.HMI.toString(), Executors.newSingleThreadExecutor());
             put(MessageName.BCM1.toString(), Executors.newSingleThreadExecutor());
@@ -38,7 +40,6 @@ public class Handler {
             put(MessageName.AD4.toString(), Executors.newSingleThreadExecutor());
         }
     };
-    private final ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor(); // 发送线程池，保持发送顺序
 
 
     // 初始化
@@ -54,18 +55,17 @@ public class Handler {
     }
 
     // 向服务器发送消息
-    private void sendData(final String json) {
-        JSONObject jsonObject = JSON.parseObject(json);
-        String msgName = jsonObject.getString("msg_name");
-        if (MapThreadPool.containsKey(msgName)) {
-            MapThreadPool.get(msgName).execute(() -> {
+    private void sendData(final String msg_name) {
+        if (MapThreadPool.containsKey(msg_name)) {
+            MapThreadPool.get(msg_name).execute(() -> {
                 try {
                     // 获取需要发送的Byte
-                    byte[] bytes = dataConvert.getByte(msgName);
+                    byte[] bytes = dataConvert.getByte(msg_name);
                     DatagramPacket datagramPacket;
                     DatagramSocket datagramSocket = new DatagramSocket();
-                    datagramPacket = new DatagramPacket(bytes, bytes.length, InetAddress.getByName(CANIp), sendPort);
+                    datagramPacket = new DatagramPacket(bytes, bytes.length, InetAddress.getByName(ServiceIp), sendPort);
                     datagramSocket.send(datagramPacket);
+                    System.out.println("Send->" + ServiceIp + ": " + ByteUtil.bytesToHex(bytes));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -74,8 +74,8 @@ public class Handler {
     }
 
     // 修改信号值
-    private void setSignalValue(String msgName, String signalName, double value){
-        dataConvert.setSignalValue(msgName,signalName,value);
+    private void setSignalValue(String msgName, String signalName, double value) {
+        dataConvert.setSignalValue(msgName, signalName, value);
     }
 
     // 接收UI界面发来的指令
@@ -93,6 +93,22 @@ public class Handler {
                 setSignalValue(data.getString("msg_name"), data.getString("signal_name"), data.getDoubleValue("value"));
                 break;
         }
+    }
+
+    // 发送修改信号值数据
+    public void modifyCommand(String msg_name, String signal_name, double value) {
+        dataConvert.setSignalValue(msg_name, signal_name, value);
+    }
+
+    // 发送send数据
+    public void sendCommand(String msg_name) {
+        sendData(msg_name);
+    }
+
+    // 发送ModifyandSend数据
+    public void modifyAndSend(String msg_name, String signal_name, double value) {
+        modifyCommand(msg_name, signal_name, value);
+        sendCommand(msg_name);
     }
 
     // 接收主机发送的消息
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,22 @@
 import eventbus.MyEventBus;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import static huat.wubeibei.candataconvert.command.MessageName.BCM1;
+import static huat.wubeibei.candataconvert.command.MessageName.VCU1;
+import static huat.wubeibei.candataconvert.command.SignalName.*;
+import static java.lang.System.exit;
 
 public class Main extends Application {
     private Handler handler;
@@ -23,6 +33,15 @@ public class Main extends Application {
         MyEventBus.register(handler);
         MyEventBus.register(this);
         initUI(primaryStage);
+        addEvent();
+        addListener();
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        exit(0);
     }
 
     private void initUI(Stage primaryStage) {
@@ -32,13 +51,8 @@ public class Main extends Application {
         initMid(root);
         Scene scene = new Scene(root, 470, 180);
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.show();
     }
 
-    private RadioButton conditionCold = new RadioButton(), conditionWarm = new RadioButton(), conditionClose = new RadioButton();
-    private TextField remainMile = new TextField(), Battery = new TextField();
-    private Slider blowingLevel = new Slider();
     private void initTop(BorderPane root) {
         HBox hBox = new HBox();
         hBox.setStyle("-fx-background-color: #991f27;"); //背景色
@@ -49,7 +63,7 @@ public class Main extends Application {
         HBox rightBottom = new HBox();
         HBox leftTop = new HBox();
         HBox leftBottom = new HBox();
-        hBox.getChildren().addAll(left,right);
+        hBox.getChildren().addAll(left, right);
         // 给左右盒子添加按钮
         left.setPadding(new Insets(10, 10, 10, 10)); //节点到边缘的距离
         left.setSpacing(10); //节点之间的间距
@@ -75,23 +89,20 @@ public class Main extends Application {
         remainMile.setMinWidth(100);
         Battery.setMinWidth(100);
         blowingLevel.setMin(0);
-        blowingLevel.setMax(100);
-        blowingLevel.setValue(40);
+        blowingLevel.setMax(5);
+        blowingLevel.setValue(0);
         blowingLevel.setShowTickLabels(true);
-        blowingLevel.setShowTickMarks(true);
-        blowingLevel.setMajorTickUnit(50);
-        blowingLevel.setMinorTickCount(5);
-        blowingLevel.setBlockIncrement(10);
+        blowingLevel.setShowTickMarks(false);
+        blowingLevel.setMajorTickUnit(1);
+        blowingLevel.setSnapToTicks(true);
+        blowingLevel.setBlockIncrement(1);
+        blowingLevel.setMinorTickCount(0);
+        blowingLevel.setBlockIncrement(1);
         blowingLevel.setMinWidth(150);
         // 添加到主面板
         root.setTop(hBox);
     }
 
-    private ToggleButton HighBeam = new ToggleButton("远光灯"), LowBeam = new ToggleButton("近光灯");
-    private ToggleButton LeftTurningLamp = new ToggleButton("左转灯"), RightTurningLamp = new ToggleButton("右转灯");
-    private ToggleButton FrontFogLamp = new ToggleButton("前雾灯"), RearFogLamp = new ToggleButton("后雾灯");
-    private ToggleButton demisterControl = new ToggleButton("除雾");
-    private TextField minSpeed = new TextField(), maxSpeed = new TextField();
     private void initMid(BorderPane root) {
         HBox hBox = new HBox();
         hBox.setStyle("-fx-background-color: #991f27;"); //背景色
@@ -101,26 +112,26 @@ public class Main extends Application {
         HBox right = new HBox();
         HBox leftTop = new HBox();
         HBox leftBottom = new HBox();
-        hBox.getChildren().addAll(left,mid, right);
+        hBox.getChildren().addAll(left, mid, right);
         // 给左右盒子添加按钮
         left.setPadding(new Insets(10, 10, 10, 10)); //节点到边缘的距离
         left.setSpacing(10); //节点之间的间距
         left.setStyle("-fx-background-color: #336699;"); //背景色
-        leftTop.getChildren().addAll(HighBeam,  LeftTurningLamp, FrontFogLamp);
-        leftBottom.getChildren().addAll(LowBeam,  RightTurningLamp, RearFogLamp);
+        leftTop.getChildren().addAll(HighBeam, LeftTurningLamp, FrontFogLamp);
+        leftBottom.getChildren().addAll(LowBeam, RightTurningLamp, RearFogLamp);
         leftTop.setSpacing(10);
         leftBottom.setSpacing(10);
         left.getChildren().addAll(leftTop, leftBottom);
         mid.setPadding(new Insets(10, 10, 10, 10)); //节点到边缘的距离
         mid.setStyle("-fx-background-color: #336699;"); //背景色
-        mid.setCenter(demisterControl);
-        right.setPadding(new Insets(10, 5, 10, 10));
+        mid.setCenter(DangerAlarmLamp);
+        right.setPadding(new Insets(10, 5, 10, 15));
         right.setStyle("-fx-background-color: #336699;"); //背景色
         right.setSpacing(10);
         right.setAlignment(Pos.CENTER);
-        right.getChildren().addAll(minSpeed, new Label("~") , maxSpeed);
-        minSpeed.setMaxWidth(85);
-        maxSpeed.setMaxWidth(85);
+        right.getChildren().addAll(minSpeed, new Label("~"), maxSpeed);
+        minSpeed.setMaxWidth(70);
+        maxSpeed.setMaxWidth(70);
         // 添加到Group
         ToggleGroup g1 = new ToggleGroup();
         ToggleGroup g2 = new ToggleGroup();
@@ -128,7 +139,78 @@ public class Main extends Application {
         LowBeam.setToggleGroup(g1);
         LeftTurningLamp.setToggleGroup(g2);
         RightTurningLamp.setToggleGroup(g2);
+        //添加边框
         // 添加到主面板
         root.setCenter(hBox);
+    }
+
+    private RadioButton conditionCold = new RadioButton(), conditionWarm = new RadioButton(), conditionClose = new RadioButton();
+    private TextField remainMile = new TextField(), Battery = new TextField();
+    private Slider blowingLevel = new Slider();
+    private ToggleButton HighBeam = new ToggleButton("远光灯"), LowBeam = new ToggleButton("近光灯");
+    private ToggleButton LeftTurningLamp = new ToggleButton("左转灯"), RightTurningLamp = new ToggleButton("右转灯");
+    private ToggleButton FrontFogLamp = new ToggleButton("前雾灯"), RearFogLamp = new ToggleButton("后雾灯");
+    private ToggleButton DangerAlarmLamp = new ToggleButton("危险警报");
+    private TextField minSpeed = new TextField(), maxSpeed = new TextField();
+
+    // 添加监听
+    private void addListener() {
+        // 空调的三个选项
+        conditionCold.setOnAction(new conditionListener());
+        conditionWarm.setOnAction(new conditionListener());
+        conditionClose.setOnAction(new conditionListener());
+        // 七个灯
+        HighBeam.setOnAction(new lampListener());
+        LowBeam.setOnAction(new lampListener());
+        LeftTurningLamp.setOnAction(new lampListener());
+        RightTurningLamp.setOnAction(new lampListener());
+        FrontFogLamp.setOnAction(new lampListener());
+        RearFogLamp.setOnAction(new lampListener());
+        DangerAlarmLamp.setOnAction(new lampListener());
+        // 空调风速
+        blowingLevel.valueProperty().addListener((ov, old_val, new_val) -> {
+            if(!conditionClose.isSelected()){
+                handler.modifyAndSend(BCM1.toString(), BCM_ACBlowingLevel.toString(), new_val.intValue());
+            }
+        });
+    }
+
+    private class conditionListener implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent event) {
+            double value = 3;
+            if (conditionClose.isSelected()) {
+                value = 2;
+            } else if (conditionCold.isSelected()) {
+                value = 0;
+            } else if (conditionWarm.isSelected()) {
+                value = 1;
+            }
+            handler.modifyAndSend(VCU1.toString(), VCU_ACWorkingStatus.toString(), value);
+        }
+    }
+
+    private class lampListener implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent event) {
+            handler.modifyCommand(BCM1.toString(), BCM_Flg_Stat_LeftTurningLamp.toString(), boolToInt(LeftTurningLamp.isSelected()));
+            handler.modifyCommand(BCM1.toString(), BCM_Flg_Stat_RightTurningLamp.toString(), boolToInt(RightTurningLamp.isSelected()));
+            handler.modifyCommand(BCM1.toString(), BCM_Flg_Stat_HighBeam.toString(), boolToInt(HighBeam.isSelected()));
+            handler.modifyCommand(BCM1.toString(), BCM_Flg_Stat_LowBeam.toString(), boolToInt(LowBeam.isSelected()));
+            handler.modifyCommand(BCM1.toString(), BCM_Flg_Stat_FrontFogLamp.toString(), boolToInt(FrontFogLamp.isSelected()));
+            handler.modifyCommand(BCM1.toString(), BCM_Flg_Stat_RearFogLamp.toString(), boolToInt(RearFogLamp.isSelected()));
+            handler.modifyCommand(BCM1.toString(), BCM_Flg_Stat_DangerAlarmLamp.toString(), boolToInt(DangerAlarmLamp.isSelected()));
+            handler.sendCommand(BCM1.toString());
+        }
+    }
+
+    // 添加事件
+    private void addEvent() {
+
+    }
+
+    private int boolToInt(boolean value) {
+        return value ? 1 : 0;
     }
 }
